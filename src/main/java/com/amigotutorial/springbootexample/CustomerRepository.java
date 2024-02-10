035dd31b892f72c9a7f59eb4b49eb1945ecf04e3
Laksara <40088875+Laksara1996@users.noreply.github.com>
2024-02-10 11:53:26 +0530

    [] - initial commit

--- src/main/java/com/amigotutorial/springbootexample/CustomerRepository.java
+++ src/main/java/com/amigotutorial/springbootexample/CustomerRepository.java
@@ -1,2 +1,6 @@
-package com.amigotutorial.springbootexample;public interface CustomerRepository {
+package com.amigotutorial.springbootexample;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 }
